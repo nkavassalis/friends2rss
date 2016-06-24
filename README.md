99bbadda983f2751bfc78464e89169e2f5cc981c
nick <nick@evul.net>
2016-06-24 14:17:24 -0400

    Some code.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # friends2rss
+
+
+This is a quick 30 minute hack of a script to generate an RSS feed from your reddit friends list. I've been trying to spend less time on Reddit and still keep track of what a couple interesting people do. Reddit generates RSS for most URLs (http://reddit.com/url/.rss) but the friends list requires authentication to access. While following specific subreddits often works for me, it doesn't let me isolate the posts from specific people. This script uses the API's oAuth authentication and converts a JSON blob to a very hand made RSS feed. This could probably be cleaned up a lot but it seems to work well enough for me!
+
+Requires Python 2.7 with the requests and json packages. Tested on Python 2.7.10 Amazon Linux and Python 2.7.11 OSX. YMMV
+
+I personally cron it on a webserver to run every 15 minutes */15 * * * * python /path/to/friends2rss.py > /some/web/path/myfriends.rss and point my reader to the live URL. You could also run it live via CGI.
+
+To use this you'll need to create an app on Reddit. Go to https://www.reddit.com/prefs/apps/ and create an app. Give it any name, chose "script" as type and enter http://127.0.0.1 as the redirect url (it's unused). See the below screenshot for an example (note this isn't a real application and its client id and secret will not work for you)
+
+![example of a reddit application](readme.png)
+
+Before using the script, fill in the four variables at the top. Your app's client id and its secret from above, aswell as your reddit user and password. It makes an oAuth request on every run.
+
+clientid = ""
+secret = ""
+reddituser = ""
+redditpass = ""
+
+Happy (less) Redditing.
---
+++ friends2rss.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+import requests
+import requests.auth
+import json
+import datetime
+import sys
+reload(sys)
+
+# fill me in!
+clientid = ""
+secret = ""
+reddituser = ""
+redditpass = ""
+
+# set UTF-8, otherwise emoji being written to a file via stdout or pipe may barf 
+sys.setdefaultencoding("UTF-8")
+
+# get an auth token from reddit that we'll use to pull our friends list
+# reddit has deprecated the non oAuth API methods
+client_auth = requests.auth.HTTPBasicAuth(clientid, secret)
+post_data = {"grant_type": "password", "username": reddituser, "password": redditpass}
+headers = {"User-Agent": "friends2rss/1.0 by nkavassalis"}
+response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
+authresponse = response.json()
+
+# get our friends submissions using the oAuth token we just got
+# output is in JSON, most recent 25 submissions
+auth = "%s %s" % (authresponse['token_type'], authresponse['access_token'])
+headers = {"Authorization": auth, "User-Agent": "friends2rss/1.0 by nkavassalis"}
+response = requests.get("https://oauth.reddit.com/%s/.json" % path, headers=headers)
+friends = response.json()['data']['children']
+
+# lets create some lazy RSS
+print "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<rss version=\"2.0\">"
+print "<channel>\n\t<title>%s's Reddit friends feed</title>\n\t<link>http://reddit.com/r/friends</link>\n\t<description>%s's Reddit friends feed</description>" % (reddituser, reddituser)
+
+for post in friends:
+  print "\t<item>"
+  print "\t\t<title>%s</title>" % post['data']['title']
+  print "\t\t<category>%s</category>" % post['data']['subreddit']
+  print "\t\t<author>%s@reddit (%s)</author>" % (post['data']['author'], post['data']['author'])
+  print "\t\t<link>%s</link>" % post['data']['url']
+
+  permalink = "https://reddit.com%s" % post['data']['permalink']
+
+  if not post['data']['selftext_html'] and post['data']['media']:
+    print "\t\t<description>&lt;a href='%s'&gt;&lt;img src='%s'&gt;&lt;/a&gt;&ltbr&gt;&lt;a href='%s'&gt;%s&lt;/a&gt;</description>" % (post['data']['url'], post['data']['media']['oembed']['thumbnail_url'], permalink, permalink)
+  elif not post['data']['selftext_html']:
+   print "\t\t<description>&lt;a href='%s'&gt;%s&lt;/a&gt;&ltbr&gt;&lt;a href='%s'&gt;%s&lt;/a&gt;</description>" % (post['data']['url'], post['data']['url'], permalink, permalink)
+  else:
+    print "\t\t<description>%s&ltbr&gt;&lt;a href='%s'&gt;%s&lt;/a&gt;</description>" % (post['data']['selftext_html'], permalink, permalink)
+
+  print "\t\t<pubDate>%s</pubDate>" % datetime.datetime.fromtimestamp(int(post['data']['created_utc'])).strftime("%a, %d %b %Y %H:%M:%S +0000")
+  print "\t</item>"
+
+print "</channel>\n</rss>"
+
